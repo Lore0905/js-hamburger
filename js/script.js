2ab833f2d60805d59b858fc0afe097b7fbd30ce8
Lore0905 <lore.castelli4@gmail.com>
2021-11-15 16:46:41 +0100

    fatto (da modificare)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-alert("cio")
+
+// seleziono le classi che dovrò modificare
+const burgerMenu = document.querySelector('.fa-bars');
+
+
+burgerMenu.addEventListener('click',
+    function() {
+        let navBurger = document.querySelector('.hamburger-menu');
+        navBurger.classList.add('d-block');
+    }
+);
+
+// seleziono le classi che dovrò modificare
+const closeBurgerMenu = document.querySelector('.close');
+
+
+closeBurgerMenu.addEventListener('click',
+    function() {
+        let navBurger = document.querySelector('.hamburger-menu');
+        navBurger.classList.add('d-none');
+    }
+);
